90bb2621979c87772ad5ae1951bd91b2fc18cddb
wangyuxiang <615418349@qq.com>
2022-04-01 21:08:44 +0800

    用torch写了两个分布，以及度量两个分布生成的样本之间距离的函数。

--- main.py
+++ main.py
@@ -8,15 +8,41 @@ We define Loss func as 'MLE' between svpg output and target output.
 import torch
 import numpy as np
 from torch.distributions.multivariate_normal import MultivariateNormal
-
+from torch.distributions.uniform import Uniform
+from torch.distributions.kl import kl_divergence
 dimension = 5
-mean_vector = [np.float64(i) for i in range(dimension)]
+n = 100
+mean_vector = [np.float32(i - 2) for i in range(dimension)]
+
+target_distribution = MultivariateNormal(torch.tensor(mean_vector), torch.eye(dimension))
+uniform = Uniform(-1, 1)
 
 # Get target distribution
-target_distribution = MultivariateNormal(torch.tensor(mean_vector), torch.eye(dimension))
-y = target_distribution.sample()
-print(y)
+y = target_distribution.rsample((n,))
+y2 = uniform.rsample((n,))
+# print(y, y.shape, y.dtype)
+
 
 # Get random input
-x = np.random.uniform(-1, 1, dimension)
-print(x, x.dtype)
+x = uniform.sample((n, dimension))
+# print(x, x.shape, x.dtype)
+
+
+# Calculate difference of my dist and target dist samples
+def distances_between_two_samples(x, y):
+    diff = 0.
+    assert len(x) == len(y)
+    for _x, _y in zip(x, y):
+        diff += (_x - _y).pow(2).sum()
+    diff /= len(x)
+    return diff
+
+
+diff_x_y = distances_between_two_samples(x, y)
+diff_x_y2 = distances_between_two_samples(x, y2)
+print("Difference between x and y is: {0}".format(diff_x_y))
+print("Difference between x and y is: {0}".format(diff_x_y2))
+
+
+# TODO: 目标是调用SVPG模块，生成样本与目标分布生成的样本越接近越好
+
